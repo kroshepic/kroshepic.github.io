0cf078eefa492f1a4620eb3d9863f44d33c71f2d
kroshepic <157121731+kroshepic@users.noreply.github.com>
2024-11-26 16:01:41 +0300

    Delete src directory

--- src/js/brands.js
+++
@@ -1,7 +0,0 @@
-// Добавим открытие модального окна для брендов при клике
-
-import { main } from './nav-links.js';
-import { brands, brandsItem } from './swiper-brands.js';
-
-//console.log(brandsItem);
-
